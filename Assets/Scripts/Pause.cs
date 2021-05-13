970384393854cf820207d58f80143073bc2303b3
ArtemDadashjants <tfastgame@gmail.com>
2021-05-13 18:17:37 +0300

    Adding game lost screen, and pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    private bool show = false;
+    public GameObject PauseObj;
+    public Text button;
+    public void ShowAndUnShow()
+    {
+        if (show != true)
+        {
+            show = true;
+            Time.timeScale = 0;
+            PauseObj.SetActive(true);
+            button.text = "Resume";
+        }
+        else
+        {
+            show = false;
+            Time.timeScale = 1;
+            PauseObj.SetActive(false);
+            button.text = "Pause";
+        }
+    }
+}
--- Assets/Scripts/restarter.cs
+++ Assets/Scripts/restarter.cs
@@ -2,14 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class restarter : MonoBehaviour
 {
+    public GameObject MenuRestart;
+    public Text EndScores;
+    public Counter score;
+    public Text CurScore;
+    public GameObject hide1;
+    public GameObject hide2;
+
     public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            EndScores.text = PlayerPrefs.GetInt("Score").ToString();
+            CurScore.text = score.ScoreText.ToString();
+            int scoresToSave = int.Parse(score.ScoreText.ToString());
+            if(scoresToSave > PlayerPrefs.GetInt("Score"))
+            {
+                PlayerPrefs.SetInt("Score", scoresToSave);
+            }
+            MenuRestart.SetActive(true);
         }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
